b273bf111aa491f3d75be00161eaf8fb76640b80
Bethanie <doloresdadd@gmail.com>
2018-07-18 13:42:09 +1000

    heroku setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
 const bodyParser = require ('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items.js');
 
@@ -19,6 +20,15 @@ mongoose.connect(db)
 
 //Use Routes
 app.use('/api/items', items);
+
+// Serve static assets of in prod
+if(process.env.NODE.ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
